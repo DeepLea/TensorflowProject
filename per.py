c2833e8a951a936dc01e3c4ea40865108b46902a
Paeron <perjaa@kth.se>
2017-05-14 20:08:23 +0200

    läser in alla bilder och delar upp bättre.

--- per.py
+++ per.py
@@ -8,21 +8,30 @@ text = namefile.readlines()
 testData = list()
 validationData = list()
 trainingData = list()
+names = list()
 
 print("************************\nPeople in this file:")
 
 for number in range(0,len(text)):
+
 			temp = text[number]
 			t = temp.split()
+			names.append(t)
 			tt = t[0].split("_")
 			print(tt[0] + " " + tt[1])
-			for image in range(1,3):
-						i = open("lfw/" + t[0] + "/" + t[0] + "_000" + str(image) + ".jpg")
-						if image == 1:
+			for image in range(1,int(t[1])):
+						if image <= 9:
+							i = open("lfw/" + t[0] + "/" + t[0] + "_000" + str(image) + ".jpg")
+						elif image >= 10 and image <= 99:
+							i = open("lfw/" + t[0] + "/" + t[0] + "_00" + str(image) + ".jpg")
+						else:
+							i = open("lfw/" + t[0] + "/" + t[0] + "_0" + str(image) + ".jpg")
+						
+						if image >= int(t[1])*0.5:
 									trainingData.append(i)
-						if image == 2:
+						elif image >= int(t[1])*0.25:
 									validationData.append(i)
-						if image == 3:
+						else:
 									testData.append(i)
 
 print("************************")
@@ -32,4 +41,25 @@ namefile.close()
 random.shuffle(trainingData)
 random.shuffle(validationData)
 random.shuffle(testData)
+
+print("Length trainingData:   " + str(len(trainingData)))
+print("Length validationData: " + str(len(validationData)))
+print("Length testData:       " + str(len(testData)))
+
+print("************************")
+
+print("Not training yet")
+
+print("************************")
+
+accuracy = 0;
+print("Accuracy is: " + str(accuracy) + " %")
+
+print("************************")
+
+
+
+
+
+
 
