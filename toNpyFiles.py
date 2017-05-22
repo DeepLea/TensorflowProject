7a4f14b2935602d009a96e90c55e2faf5f84705e
Paeron <perjaa@kth.se>
2017-05-22 12:49:20 +0200

    small fixes

--- toNpyFiles.py
+++ toNpyFiles.py
@@ -2,8 +2,7 @@ import tensorflow.contrib.learn as skflow
 from sklearn.datasets import fetch_lfw_people
 import numpy as np
 
-#TODO set min faces per person to 10
-lfw_people = fetch_lfw_people(min_faces_per_person = 70, resize = 0.88, funneled = False,color = False, slice_ = (slice(0,250),slice(0,250)))  # Det har tar tid, upp till 2 min
+lfw_people = fetch_lfw_people(min_faces_per_person = 50, resize = 0.88, funneled = False,color = False, slice_ = (slice(0,250),slice(0,250)))  # Det har tar tid, upp till 2 min
 
 np.save("data.npy",lfw_people.data)
 np.save("target.npy",lfw_people.target)
